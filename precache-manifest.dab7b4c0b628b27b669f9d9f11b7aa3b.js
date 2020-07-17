8957af5954e9439e1652602b8aa67b4276275897
Iman-Budi Pranakaish <ibpranakasih@gmail.com>
2020-07-17 19:02:04 +0700

    Updates

---
+++ precache-manifest.dab7b4c0b628b27b669f9d9f11b7aa3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05bb1c3cd2709cc8f0b5f640fa159937",
+    "url": "/pokercash-client/index.html"
+  },
+  {
+    "revision": "cfebc6778e007b3875d1",
+    "url": "/pokercash-client/static/css/main.ebda5a02.chunk.css"
+  },
+  {
+    "revision": "d7cf5be4202e299df573",
+    "url": "/pokercash-client/static/js/2.d348f351.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokercash-client/static/js/2.d348f351.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfebc6778e007b3875d1",
+    "url": "/pokercash-client/static/js/main.53a2e145.chunk.js"
+  },
+  {
+    "revision": "8d273b31c46022fa62e0",
+    "url": "/pokercash-client/static/js/runtime-main.23c78657.js"
+  }
+]);
